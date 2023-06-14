82ab5c23d6a996ca0b4f84e4057d6bf309b7166d
Shahi77 <shahiinfinity02@gmail.com>
2023-06-14 12:34:00 +0530

    creating page routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/Header';
+import { Homepage } from './Pages/Homepage';
+import { CoinPage } from './Pages/CoinPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} />
       </div>
     </BrowserRouter>
   );
